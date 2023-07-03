9229a8166237114ce0b727ce47fb7fe7e396e047
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-07-03 09:01:48 +0600

    feat: add microsoft office activation script

---
+++ Windows/activate-microsoft-office.bat
@@ -0,0 +1,9 @@
+REM https://github.com/21Z/Microsoft-Office-2021#install-office-2021-using-office-deployment-tool
+
+if exist "C:\Program Files\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files\Microsoft Office\Office16"
+if exist "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files (x86)\Microsoft Office\Office16"
+for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:kms.msgang.com
+cscript ospp.vbs /act
+pause
